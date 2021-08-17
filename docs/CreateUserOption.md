aa8dd22bdca440ef647d464a873e070fd520100b
Jakob Lykke Andersen <Jakob@caput.dk>
2021-08-17 14:56:38 +0200

    Update to v1.15-rc3

--- docs/CreateUserOption.md
+++ docs/CreateUserOption.md
@@ -11,6 +11,7 @@ Name | Type | Description | Notes
 **send_notify** | **bool** |  | [optional] 
 **source_id** | **int** |  | [optional] 
 **username** | **str** |  | 
+**visibility** | **str** |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- giteapy/api/__init__.py
+++ giteapy/api/__init__.py
@@ -3,9 +3,11 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from giteapy.api.admin_api import AdminApi
-from giteapy.api.issue_api import IssueApi
-from giteapy.api.miscellaneous_api import MiscellaneousApi
-from giteapy.api.organization_api import OrganizationApi
-from giteapy.api.repository_api import RepositoryApi
-from giteapy.api.user_api import UserApi
+from .admin_api import AdminApi
+from .issue_api import IssueApi
+from .miscellaneous_api import MiscellaneousApi
+from .notification_api import NotificationApi
+from .organization_api import OrganizationApi
+from .repository_api import RepositoryApi
+from .settings_api import SettingsApi
+from .user_api import UserApi
